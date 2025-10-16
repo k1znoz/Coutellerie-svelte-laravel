dbb2e9186906f7b8d2da195fd694f919bac9bfd1
Kiznoz <alois.sautet@gmail.com>
2025-10-16 18:21:27 +0200

    commit original 2em build ?

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'coutellerie/svelte-laravel',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6735370e857a22ba182c5c8003e7f5c60c8e0fc1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'coutellerie/svelte-laravel' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6735370e857a22ba182c5c8003e7f5c60c8e0fc1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
