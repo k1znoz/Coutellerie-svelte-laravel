ac87d1d8a016a7d4bfd5a747f103acc083a171e9
Kiznoz <alois.sautet@gmail.com>
2025-10-18 13:35:52 +0200

    La page n'est pas redirigée correctement

--- services/coutellerie-laravel/routes/web.php
+++ services/coutellerie-laravel/routes/web.php
@@ -7,11 +7,6 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
-// Route login - Redirection vers l'authentification Filament (avec nom)
-Route::get('/login', function () {
-    return redirect('/admin');
-})->name('login');
-
 // Route API info (accessible via /api-info)
 Route::get('/api-info', function () {
     return response()->json([
